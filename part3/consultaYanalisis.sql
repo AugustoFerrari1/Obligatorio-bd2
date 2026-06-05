721676bf6c5084c7499118d03bb224973e199104
Augusto <augustoferrari100@gmail.com>
2026-06-05 13:39:20 -0300

    Nueva tabla de explain plan

--- part3/consultaYanalisis.sql
+++ part3/consultaYanalisis.sql
@@ -41,40 +41,45 @@ SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY);
 
 -- RESULTADOS - export.xml es el archivo dado por oracle con el plan
 /*
-Plan hash value: 165572356
+Plan hash value: 4221167918
 
--------------------------------------------------------------------------------------------
-| Id  | Operation                 | Name          | Rows  | Bytes | Cost (%CPU)| Time     |
--------------------------------------------------------------------------------------------
-|   0 | SELECT STATEMENT          |               |     4 |  1520 |    20  (10)| 00:00:01 |
-|   1 |  SORT ORDER BY            |               |     4 |  1520 |    20  (10)| 00:00:01 |
-|*  2 |   FILTER                  |               |       |       |            |          |
-|   3 |    HASH GROUP BY          |               |     4 |  1520 |    20  (10)| 00:00:01 |
-|*  4 |     HASH JOIN             |               |     4 |  1520 |    18   (0)| 00:00:01 |
-|*  5 |      HASH JOIN            |               |     4 |  1204 |    15   (0)| 00:00:01 |
-|*  6 |       HASH JOIN           |               |     4 |   664 |    12   (0)| 00:00:01 |
-|*  7 |        HASH JOIN          |               |     4 |   560 |     9   (0)| 00:00:01 |
-|*  8 |         HASH JOIN         |               |     4 |   456 |     6   (0)| 00:00:01 |
-|*  9 |          TABLE ACCESS FULL| COMUNIDAD     |     3 |   204 |     3   (0)| 00:00:01 |
-|* 10 |          TABLE ACCESS FULL| PUBLICACION   |     4 |   184 |     3   (0)| 00:00:01 |
-|  11 |         TABLE ACCESS FULL | COMENTARIO    |     4 |   104 |     3   (0)| 00:00:01 |
-|  12 |        TABLE ACCESS FULL  | CONTENIDO     |    11 |   286 |     3   (0)| 00:00:01 |
-|* 13 |       TABLE ACCESS FULL   | AGENTE        |     6 |   810 |     3   (0)| 00:00:01 |
-|  14 |      TABLE ACCESS FULL    | USUARIOHUMANO |     5 |   395 |     3   (0)| 00:00:01 |
--------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------
+| Id  | Operation                            | Name             | Rows  | Bytes | Cost (%CPU)| Time     |
+---------------------------------------------------------------------------------------------------------
+|   0 | SELECT STATEMENT                     |                  |     4 |  1520 |    20  (10)| 00:00:01 |
+|   1 |  SORT ORDER BY                       |                  |     4 |  1520 |    20  (10)| 00:00:01 |
+|*  2 |   FILTER                             |                  |       |       |            |          |
+|   3 |    HASH GROUP BY                     |                  |     4 |  1520 |    20  (10)| 00:00:01 |
+|*  4 |     HASH JOIN                        |                  |     4 |  1520 |    18   (0)| 00:00:01 |
+|   5 |      NESTED LOOPS                    |                  |     3 |  1062 |    15   (0)| 00:00:01 |
+|   6 |       NESTED LOOPS                   |                  |     3 |  1062 |    15   (0)| 00:00:01 |
+|   7 |        NESTED LOOPS                  |                  |     3 |   825 |    12   (0)| 00:00:01 |
+|   8 |         NESTED LOOPS                 |                  |     3 |   420 |     9   (0)| 00:00:01 |
+|   9 |          NESTED LOOPS                |                  |     3 |   216 |     6   (0)| 00:00:01 |
+|* 10 |           TABLE ACCESS FULL          | PUBLICACION      |     3 |   138 |     3   (0)| 00:00:01 |
+|  11 |           TABLE ACCESS BY INDEX ROWID| CONTENIDO        |     1 |    26 |     1   (0)| 00:00:01 |
+|* 12 |            INDEX UNIQUE SCAN         | PK_CONTENIDO     |     1 |       |     0   (0)| 00:00:01 |
+|* 13 |          TABLE ACCESS BY INDEX ROWID | COMUNIDAD        |     1 |    68 |     1   (0)| 00:00:01 |
+|* 14 |           INDEX UNIQUE SCAN          | PK_COMUNIDAD     |     1 |       |     0   (0)| 00:00:01 |
+|* 15 |         TABLE ACCESS BY INDEX ROWID  | AGENTE           |     1 |   135 |     1   (0)| 00:00:01 |
+|* 16 |          INDEX UNIQUE SCAN           | PK_AGENTE        |     1 |       |     0   (0)| 00:00:01 |
+|* 17 |        INDEX UNIQUE SCAN             | PK_USUARIOHUMANO |     1 |       |     0   (0)| 00:00:01 |
+|  18 |       TABLE ACCESS BY INDEX ROWID    | USUARIOHUMANO    |     1 |    79 |     1   (0)| 00:00:01 |
+|  19 |      TABLE ACCESS FULL               | COMENTARIO       |     4 |   104 |     3   (0)| 00:00:01 |
+---------------------------------------------------------------------------------------------------------
 
 Predicate Information (identified by operation id):
 ---------------------------------------------------
 
    2 - filter(SUM("P"."VOTOSTOTALES")>0)
-   4 - access("UH"."EMAIL"="A"."EMAILADMIN")
-   5 - access("A"."IDAGENTE"="C"."IDAGENTE")
-   6 - access("C"."IDCONTENIDO"="P"."IDCONTENIDO")
-   7 - access("CM"."IDPUBLICACION"="P"."IDCONTENIDO")
-   8 - access("COM"."IDCOMUNIDAD"="P"."IDCOMUNIDAD")
-   9 - filter("COM"."ARCHIVADO"='N')
+   4 - access("CM"."IDPUBLICACION"="P"."IDCONTENIDO")
   10 - filter("P"."ESTADO"='Activa')
-  13 - filter("A"."ESTADO"='Activo')
+  12 - access("C"."IDCONTENIDO"="P"."IDCONTENIDO")
+  13 - filter("COM"."ARCHIVADO"='N')
+  14 - access("COM"."IDCOMUNIDAD"="P"."IDCOMUNIDAD")
+  15 - filter("A"."ESTADO"='Activo')
+  16 - access("A"."IDAGENTE"="C"."IDAGENTE")
+  17 - access("UH"."EMAIL"="A"."EMAILADMIN")
 
 Note
 -----
