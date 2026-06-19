e4d1134ccc4afbc64a825a33369fc891b5851a4d
Augusto <augustoferrari100@gmail.com>
2026-06-19 15:04:13 -0300

    Subo json

---
+++ testDeTodasLasPartes/Parte3/ejecicionParte3.sql
@@ -0,0 +1,13 @@
+SQL> EXPLAIN PLAN FOR
+     SELECT
+         com.nombre                              AS comunidad,
+         uh.alias                                AS admin_alias,...
+Show more...
+
+
+
+Explained.
+
+Elapsed: 00:00:00.060
+
+La tabla generada es el json que esta debajo de este .sql
